1b0e99cbf786b6e6c0eaad8ee401a7a5e4c69129
Pierbit <pierpaolocammardella@gmail.com>
2025-05-22 12:48:25 +0200

    1.1

--- queries.js
+++ queries.js
@@ -1,33 +1,25 @@
 import pool from './db.js';
 
-async function insertDailyBattleJson(collected) {
+export async function insertDailyBattleJson(collected) {
     await pool.query('INSERT INTO dailybattlesreal (report) VALUES ($1)', [collected]);
 }
 
-async function readDailyBattleJson() {
+export async function readDailyBattleJson() {
     const result = await pool.query('SELECT report FROM dailybattlesreal ORDER BY id DESC LIMIT 1;');
     return result.rows[0].report;
 }
 
-async function readLast14DailyBattleJson() {
+export async function readLast14DailyBattleJson() {
     const result = await pool.query('SELECT report FROM dailybattlesreal ORDER BY id DESC LIMIT 14;');
     return result.rows;
 }
 
-async function insertComprehensiveReport(report) {
+export async function insertComprehensiveReport(report) {
     await pool.query('DELETE FROM comprehensivereportreal');
     await pool.query('INSERT INTO comprehensivereportreal (creport) VALUES ($1)', [report]);
 }
 
-async function readComprehensiveReport() {
+export async function readComprehensiveReport() {
     const result = await pool.query('SELECT creport FROM comprehensivereportreal ORDER BY id DESC LIMIT 1;');
     return result.rows[0].creport;
 }
-
-module.exports = {
-    insertDailyBattleJson,
-    readDailyBattleJson,
-    readLast14DailyBattleJson,
-    insertComprehensiveReport,
-    readComprehensiveReport
-};
