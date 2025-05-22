b47bd317c328c6d8c66bb2ddfbe8ea0f75e1b332
Pierbit <pierpaolocammardella@gmail.com>
2025-05-22 10:21:30 +0200

    1.1

--- db.js
+++ db.js
@@ -2,11 +2,7 @@ import pg from 'pg';
 const { Pool } = pg;
 
 const pool = new Pool({
-    user: process.env.PGUSER,
-    host: process.env.PGHOST,
-    database: process.env.PGDATABASE,
-    password: process.env.PGPASSWORD,
-    port: process.env.PGPORT,
+    connectionString: process.env.db,
     ssl: { rejectUnauthorized: false }
 });
 
